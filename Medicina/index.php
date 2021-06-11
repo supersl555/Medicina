630fe78e8618ee8ea952f4244f131f63b39f9254
supersl555 <61551225+supersl555@users.noreply.github.com>
2021-06-11 19:27:51 +0600

    Add files via upload

---
+++ Medicina/index.php
@@ -0,0 +1,242 @@
+.<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>DK</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css"/>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<script href="js/jquery-3.6.0.slim.min.js"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/script.js"></script>
+</head>
+<body>
+	<header>
+		<div class="On_Shapka">
+			<small size="10px" class="On_Shapka_identy">Г.Алматы</small>
+			<small class="On_Shapka_identy">Г.Нур-Султан</small>
+		</div>
+		<div class="Shapka_main">
+			<a class="Shapka">Наш работы</a>
+			<a class="Shapka">Какие проблемы решает</a>
+			<a class="Shapka">Отзывы</a>
+			<a class="Shapka">Наши волшебники</a>
+			<a class="Shapka">Контакты</a>
+			<img>
+			<a class="Shapka_identy">Заказать звонок</a>
+			<a  class="Shapka" href="tel:+7 775 705 33 33">+7 775 705 33 33</a>
+			<a  class="Shapka" href="tel:+7 775 705 33 33">+7 775 705 33 33</a>
+			<a class="Shapka"><img></a>
+			<a class="Shapka"><img></a>
+			<a class="Shapka"><img></a>
+		</div>
+		<div class="main_text">
+			<em>Dr.Koblandin Clinic</em>
+			<h1 class="main_size">Поднимите мне веки!</h1>
+			<h1 style="margin-top:-30px;">Выгодные условия</h1>
+			<h1 style="margin-top:-20px;">блеферопласики</h1>
+			<div>
+				<img><big>Экономия до 200 000 тенге</big><br>
+				<img><big>Рассрочка до 6 месяцев без переплаты</big><br>
+				<button class="button_header">Бесплатная консультация</button>
+			</div>
+		</div>
+		<div class="img_Shapka">
+			<img src="image/Photo.png">
+		</div>
+	</header>
+	<main>
+		<img><h1 align="center">Что такое блеферопласика?</h1><img>
+		<div class="What">
+			<div class="info_main">
+				<img><b>Пластика нижних век — данная операция позволяет устранить дряблость и провисание кожи. Так же удаётся скрыть чрезмерно выделяющиеся слёзные борозды</b><br>
+				<img><b>Пластика верхних век — показанием являются грыжи, которые служат утяжелителем, из-за чего наблюдается опускание века</b><br>
+				<img><b>Круговая пластика век — вмешательство включает работу над верхними и нижними веками. С помощью данной операции удаётся добиться максимально завершённого и целостного результата</b><br>
+			</div>
+			<div style="float: left; width:40%;">
+				<img src="image/Photo (1).png" style="margin-left: 99px;">
+			</div>
+			<div class="info_main">
+				<img><b>Трансконъюнктивальная блефаропластика — позволяет решить внешние проблемы путём удаления жировиков и грыж через специальные проколы на слизистой</b><br>
+				<img><b>Кантопексия — назначается с целью поднятия наружных уголков глаз и последующей их фиксации на месте</b><br>
+				<img><b>Перераспределение жировых участков — показанием к вмешательству данного типа является очевидная асимметрия зоны лица вокруг глаз</b><br>
+			</div>
+		</div>
+		<div>
+			<img><h1 align="center">Работы наших специалистов</h1><img>
+			<div align="center">
+				<big>Подсписывайтесь на наш <big style="background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%);  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;">instagram</big></big>
+				<button class="button_our_work"><img>Подписаться</button>
+			</div>
+			<div class="block_main">
+				<div class="block_first">
+					<div class="block_second">
+						<img src="image/Rectangle 6.png"><br>
+						<img src="image/Rectangle 6.png"><br>
+					</div>
+				</div>
+				<div class="block_first">
+					<div class="block_second">
+						<img src="image/Rectangle 6.png"><br>
+						<img src="image/Rectangle 6.png"><br>
+					</div>
+				</div>
+				<div class="block_first">
+					<div class="block_second">
+						<img src="image/Rectangle 6.png"><br>
+						<img src="image/Rectangle 6.png"><br>
+					</div>
+				</div>
+				<div class="block_first">
+					<div class="block_second">
+						<img src="image/Rectangle 6.png"><br>
+						<img src="image/Rectangle 6.png"><br>
+					</div>
+				</div>
+				<div class="block_first">
+					<div class="block_second">
+						<img src="image/Rectangle 6.png"><br>
+						<img src="image/Rectangle 6.png"><br>
+					</div>
+				</div>
+				<div class="block_first">
+					<div class="block_second">
+						<img src="image/Rectangle 6.png"><br>
+						<img src="image/Rectangle 6.png"><br>
+					</div>
+				</div>
+			</div>
+			<div align="center">
+				<button class="button_header">Смотреть все работы<img></button>
+			</div>
+		</div>
+		<img><h1 align="center">Какие проблемы решает блеферопластика</h1><img>
+		<div>
+		</div>
+		<img><h1 align="center">Почему выбирают нашу клинику</h1><img>
+		<div class="Why_us">
+			<div class="our_sentence">
+				<div>
+					<span class="size_us">01</span>
+				</div>
+				<div class="about_us_text">
+					<span>Онлайн консультация бесплатно</span>
+					<p>Повседневная практика показывает...</p>
+				</div>
+			</div>
+			<div class="our_sentence">
+				<div>
+					<span class="size_us">02</span>
+				</div>
+				<div class="about_us_text">
+					<span>Онлайн консультация бесплатно</span>
+					<p>Повседневная практика показывает...</p>
+				</div>
+			</div>
+			<div class="our_sentence second_us">
+				<div>
+					<span class="size_us">03</span>
+				</div>
+				<div class="about_us_text">
+					<span>Онлайн консультация бесплатно</span>
+					<p>Повседневная практика показывает...</p>
+				</div>
+			</div>
+			<div class="our_sentence">
+				<div>
+					<span class="size_us">04</span>
+				</div>
+				<div class="about_us_text">
+					<span>Онлайн консультация бесплатно</span>
+					<p>Повседневная практика показывает...</p>
+				</div>
+			</div>
+			<div class="our_sentence">
+				<div>
+					<span class="size_us">05</span>
+				</div>
+				<div class="about_us_text">
+					<span>Онлайн консультация бесплатно</span>
+					<p>Повседневная практика показывает...</p>
+				</div>
+			</div>
+			<div class="our_sentence">
+				<div>
+					<span class="size_us">06</span>
+				</div>
+				<div class="about_us_text">
+					<span>Онлайн консультация бесплатно</span>
+					<p>Повседневная практика показывает...</p>
+				</div>
+			</div>
+		</div>
+		<img><h1 align="center">Отзывы наших клиентов</h1><img>
+		<div align="center">
+			<big>Подсписывайтесь на наш <big style="background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%);  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent">youtube</big></big><button class="button_our_work"><img>Подписаться</button>
+		</div>
+		<div class="On_slider">
+			<div class="slider">
+				<div class="slider__item">
+					<a href="#" onclick="slowScroll('#more')"><iframe width="312" height="150" src="https://www.youtube.com/embed/rXNfDfqtM3M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></a>
+				</div>
+				<div class="slider__item">
+					<a href="#" onclick="slowScroll('#more')"><iframe width="312" height="150" src="https://www.youtube.com/embed/rXNfDfqtM3M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></a>
+				</div>
+				<div class="slider__item">
+					<a href="#" onclick="slowScroll('#more')"><iframe width="312" height="150" src="https://www.youtube.com/embed/rXNfDfqtM3M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></a>
+				</div>
+				<div class="slider__item">
+					<a href="#" onclick="slowScroll('#more')"><iframe width="312" height="150" src="https://www.youtube.com/embed/rXNfDfqtM3M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></a>
+				</div>
+				<div class="slider__item">
+					<a href="#" onclick="slowScroll('#more')"><iframe width="312" height="150" src="https://www.youtube.com/embed/rXNfDfqtM3M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></a>
+				</div>
+				<div class="slider__item">
+					<a href="#" onclick="slowScroll('#more')"><iframe width="312" height="150" src="https://www.youtube.com/embed/rXNfDfqtM3M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></a>
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer>
+		<div class="main_footer">
+			<div class="img_footer">
+				<img><br>
+				<img><img><img>
+			</div>
+			<div class="info_footer">
+				<span style="font-size:20px; margin-left:20px;">Контакты</span>
+				<img>
+				<div class="Almaty">
+					<div class="in_Almaty">
+						<span>г.Алматы</span><br>
+						<span>+7 775 705 33 33</span><br>
+						<span>ул.Маркова,75,уг.Аль-Фараби</span><br>
+						<a style="border-bottom: 1px dashed white; color:white;">Смотреть на карте</a>
+					</div>
+				</div>
+				<img>
+				<div class="Nursultan">
+					<div class="in_Nursultan">
+						<span>г.Нур-Султан</span><br>
+						<span>+7 708 705 33 33</span><br>
+						<span>ул.Бухар-Жырау,20В,201НП</span><br>
+						<span style="border-bottom: 1px dashed white; color:white;">Смотреть на карте</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="map_footer">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5814.4780223783955!2d76.93761545770347!3d43.22544825352349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z0YPQuy7QnNCw0YDQutC-0LLQsCw3NSzRg9CzLtCQ0LvRjC3QpNCw0YDQsNCx0Lg!5e0!3m2!1sru!2skz!4v1623079929085!5m2!1sru!2skz" style="border:0; width:100%; height: 100%;" allowfullscreen="" loading="lazy"></iframe>
+		</div>
+		<div class="under_main_footer">
+			<img style="float:left;">
+			<h5 class="shapka_footer">Copyright 2021 - Dr.KoblandinClinic.ru</h5>
+			<h5 class="shapka_footer">All Rights Reserved</h5>
+			<h5 class="shapka_footer">Разработка сайтов</h5>
+			<h5 class="shapka_footer">intell</h5>
+			<img>
+		</div>
+	</footer>
+</body>
+</html>
